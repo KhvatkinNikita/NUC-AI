31083b798f9f5f1f962ca82b5d2f6d4ce62d9690
Nikita Khvatkin <khvatkin.nikita@gmail.com>
2025-03-10 21:20:26 +0100

    REE API retrieval

---
+++ API/structure.py
@@ -0,0 +1,13 @@
+"""
+REE API Structure
+"""
+
+JSON_routes = {
+    ('balance', 'balance-electrico') : [
+        ['included'],
+        ['attributes', 'content'],
+    ],
+    ('demanda', 'evolucion') : [
+        ['included'],
+    ]
+}
